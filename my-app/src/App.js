24385d24370e3822fec496214a1e0e5a7fa23487
Stephen Guo <wetech16@gmail.com>
2020-08-13 23:01:20 -0700

    -E2-added-reach-router
    added components for reach router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Router } from '@reach/router'
+import About from './pages/About';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Blog from './pages/Blog';
 
 function App() {
+  const navLinks = [
+		{
+			text: 'Home',
+			path: '/',
+			icon: 'ion-ios-home'
+		},
+		{
+			text: 'Contact',
+			path: '/contact',
+			icon: 'ion-ios-megaphone'
+		},
+		{
+			text: 'About',
+			path: '/about',
+			icon: 'ion-ios-business'
+		},
+		{
+			text: 'Blog',
+			path: '/blog',
+			icon: 'ion-ios-bonfire'
+		},
+		{
+			text: 'Portfolio',
+			path: '/portfolio',
+			icon: 'ion-ios-briefcase'
+		}
+  ]
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <Home path="/home" />
+          <Portfolio path="/portfolio" />
+          <Contact path="/contact" />
+          <About path="/about" />
+          <Blog path="/blog" />
+
+      </Router>
     </div>
   );
 }
